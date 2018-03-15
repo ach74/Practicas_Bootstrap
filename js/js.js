d11af16bfa3a053e7eba1037e3d314f8a4bb1886
dam1a07 <dam1a07@SINF2003SRVR2.LOCAL>
2018-03-15 19:45:32 +0100

    aaa
    aaa

---
+++ js/js.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $(boton).click(function(){
+        alert("AAAAAAAAA");
+        $('p').html("Ismael de menorca es gays");
+        $(fondo).css("background-color", "red");
+    });
+});
